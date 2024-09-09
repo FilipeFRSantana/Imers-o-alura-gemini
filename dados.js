fb84c016f9020419b747982cc99093e80f70ea9d
Filipe Fernandes <150837521+FilipeFRSantana@users.noreply.github.com>
2024-09-09 14:07:49 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,36 @@
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+    dataNascimento: "06 de setembro de 2008",
+    modalidade: "Skate",
+    titulos: ["Campeã mundial júnior, Medalha de prata nos Jogos Olímpicos de Tóquio 2020"],
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+     tags: "skate fada sk8"
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou a medalha de prata nas Olimpíadas de Tóquio 2020, sendo a primeira brasileira a conquistar uma medalha olímpica na ginástica artística. Seu talento e determinação a tornaram um ícone do esporte brasileiro. Em 2024, tornou-se campeã olímpica, superando sua maior adversária, Simone Biles.",
+    dataNascimento: "08 de maio de 1999",
+    modalidade: "Ginástica artística",
+    titulos: ["Medalha de prata nos Jogos Olímpicos de Tóquio 2020, Campeã olímpica em 2024, Primeira brasileira a conquistar uma medalha olímpica na ginástica artística"],
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginástica ginastica ouro"
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que conquistou a primeira medalha de ouro do Brasil nas Olimpíadas de 2024 em Paris, tornando-se a primeira mulher a conquistar uma medalha de ouro Olímpica na história do Esporte Clube Pinheiros.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    dataNascimento: "20 de maio de 1998",
+    modalidade: "Judô",
+    categoria: "Pesado (+78 kg)",
+    titulos: ["Medalha de ouro nos Jogos Olímpicos de 2024", "Medalha de ouro nos Jogos Pan-Americanos de 2019"],
+    clube: "Esporte Clube Pinheiros",
+    altura: "1,76 m",
+    redesSociais: {
+      instagram: "beatrizsouza875"
+    },
+    tags: "judô judo judoca judoka"
+  }
+];
+
